f9f2f101b133102428937be0f838ece0b161db82
Jake Vossen <jake@vossen.dev>
2020-02-22 14:22:05 -0700

    added python script back

---
+++ convert_py_to_api/conversion.py
@@ -0,0 +1,41 @@
+from typing import List
+import sys
+
+
+def main() -> None:
+    input_file: str = str(sys.argv[1])
+    output_file: str = str(sys.argv[2])
+
+    output_lines: List[str] = []
+
+    output_lines.append("from flask import Flask")
+    output_lines.append("app = Flask(__name__)")
+
+    with open(input_file) as f:
+        input_file_lines = f.readlines()
+
+    for line in input_file_lines:
+        # dealing with a function, we need to add a flask decorator
+        if line.startswith('def'):
+            decorator: str = get_flask_decorator(line)
+            output_lines.append(decorator)
+        output_lines.append(line)
+
+    output_lines.append("if __name__ == '__main__':")
+    output_lines.append("    app.run()")
+
+    print("printing output lines")
+    output_file = open(output_file, 'w')
+    for line in output_lines:
+        output_file.write(line + '\n' if len(line) > 0 else '')
+
+
+def get_flask_decorator(line: str):
+    # should probably do this with a regex but hackathon am i right
+    func_name = line.split('(')[0].split(' ')[1]
+    return "@app.route('/" + func_name + "')"
+
+
+print(get_flask_decorator("def hello_world()"))
+print(get_flask_decorator("def ffdfdfd(sasd fdf fdf)"))
+main()
---
+++ convert_py_to_api/example.py
@@ -0,0 +1,13 @@
+# This is an example file with a couple functions to show off as an api
+
+
+def foo():
+    return "Foo"
+
+
+def bar():
+    return "Bar"
+
+
+def foobar():
+    return foo() + bar()

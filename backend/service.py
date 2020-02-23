5fca8acf4aafbe3cc751b5e22f625393af0d9956
Jake Vossen <jake@vossen.dev>
2020-02-22 21:26:29 -0700

    basic echo of url

--- backend/service.py
+++ backend/service.py
@@ -1,17 +1,23 @@
 from flask import Flask, request
-from flask_cors import CORS
+from flask import request, jsonify
+# from flask_cors import CORS
 
 app = Flask(__name__)
-#CORS(app)
+# CORS(app)
+
 
 @app.route('/')
 def start():
     return 'server running'
 
-@app.route('/post/', methods=['POST'])
+
+@app.route('/post', methods=['POST'])
 def rec():
-    data = request.json()
-    return data['url']
+    # data = d.json
+    print(request.form.get('url'))
+    return jsonify(request.form.get('url'))
+    # return data['url']
+
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug=True)
